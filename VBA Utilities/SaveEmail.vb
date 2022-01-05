181a6b505d231d714d83df69665f21aa549dd310
Dylan Kinnett <dkinnett@thewalters.org>
2022-01-05 17:06:13 -0500

    Custom format for meeting notes

--- VBA Utilities/SaveEmail.vb
+++ VBA Utilities/SaveEmail.vb
@@ -18,101 +18,101 @@ Sub ExtractEmail()
   On Error GoTo EndClean:
   ' Establish the environment and selected items (emails)
   ' NOTE: selecting a conversation-view stack wont work
-  '     you'll need to select one of the emails
+  '   you'll need to select one of the emails
   Dim fileName As String
   Dim temporarySubjectLineString As String
   Dim currentExplorer As Explorer
-    Set currentExplorer = Application.ActiveExplorer
+  Set currentExplorer = Application.ActiveExplorer
   Dim Selection As Selection
-    Set Selection = currentExplorer.Selection
+  Set Selection = currentExplorer.Selection
   ' For each email in the Selection
   ' Assigning email item to the `obj` holding variable
   For Each obj In Selection
-    ' set the oMail object equal to that mail item
-    Set oMail = obj
-    ' Is it an Email?
-    If oMail.Class <> 43 Then
-      MsgBox "This code only works with Emails."
-      GoTo EndClean: ' you broke it
-    End If
+  ' set the oMail object equal to that mail item
+  Set oMail = obj
+  ' Is it an Email?
+  If oMail.Class <> 43 Then
+    MsgBox "This code only works with Emails."
+    GoTo EndClean: ' you broke it
+  End If
 
 
 'Extract Data from Email'
 '============================================================================='
 
-    ' Get the Subject line
-    temporarySubjectLineString = oMail.Subject
-    ' function call the name cleaner to remove any
-    '  illegal characters from the subject line
-    ReplaceCharsForFileName temporarySubjectLineString, ""
+  ' Get the Subject line
+  temporarySubjectLineString = oMail.Subject
+  ' function call the name cleaner to remove any
+  '  illegal characters from the subject line
+  ReplaceCharsForFileName temporarySubjectLineString, ""
 
-    ' List the recipients
-    Dim recips As Outlook.Recipients
-      Set recips = oMail.Recipients
-    Dim recip As Outlook.Recipient
-    Dim result As String
-    Dim recipString As String
-      recipString = ""
-    For Each recip In recips
-      recipString = recipString & "  "
-      recipString = recipString & "- "
-      recipString = recipString & formatName(recip.name, personNameStartChar)
-      ' recipString = recipString & vbCrLf
-    Next
+  ' List the recipients
+  Dim recips As Outlook.Recipients
+    Set recips = oMail.Recipients
+  Dim recip As Outlook.Recipient
+  Dim result As String
+  Dim recipString As String
+    recipString = ""
+  For Each recip In recips
+    recipString = recipString & "  "
+    recipString = recipString & "- "
+    recipString = recipString & formatName(recip.name, personNameStartChar)
+    ' recipString = recipString & vbCrLf
+  Next
 
-    ' Get sender's name and email
-    Dim sender As String
-    ' sender = formatName(oMail.sender, personNameStartChar)
-    ' sender addresses differ with internal/exchange senders
-      If oMail.SenderEmailType = "SMTP" Then
-          sender = oMail.sender & " <" & oMail.SenderEmailAddress & ">"
-      Else
-          If oMail.SenderEmailType = "EX" Then
-              sender = oMail.sender & " <" & oMail.Sender.GetExchangeUser.PrimarySmtpAddress & ">"
-          End If
+  ' Get sender's name and email
+  Dim sender As String
+  ' sender = formatName(oMail.sender, personNameStartChar)
+  ' sender addresses differ with internal/exchange senders
+    If oMail.SenderEmailType = "SMTP" Then
+      sender = oMail.sender & " <" & oMail.SenderEmailAddress & ">"
+    Else
+      If oMail.SenderEmailType = "EX" Then
+        sender = oMail.sender & " <" & oMail.Sender.GetExchangeUser.PrimarySmtpAddress & ">"
       End If
+    End If
 
-    ' Get the email recieved date
-    Dim dtDate As Date
-      dtDate = oMail.ReceivedTime
+  ' Get the email recieved date
+  Dim dtDate As Date
+    dtDate = oMail.ReceivedTime
 
 
 'Create a Markdown File'
 '============================================================================='
 
-    ' For some reason, it adds a few empty lines to the start of the file?
+  ' For some reason, it adds a few empty lines to the start of the file?
 
-    ' Make YAML Frontmatter
-    Dim emailFrontmatter As String
-    emailFrontmatter = "---" & vbCrLf
-    emailFrontmatter = emailFrontmatter & "subject: " & Chr(34) & oMail.Subject & Chr(34) & vbCrLf
-    emailFrontmatter = emailFrontmatter & "from: " & Chr(34) & sender & Chr(34) & vbCrLf
-    emailFrontmatter = emailFrontmatter & "to: " & recipString & vbCrLf
-    emailFrontmatter = emailFrontmatter & "date:" & Format(oMail.ReceivedTime, "yyyy-mm-dd") & Format(oMail.ReceivedTime, "hh:MM AM/PM") & vbCrLf
-    emailFrontmatter = emailFrontmatter & "tags: email" & vbCrLf
-    emailFrontmatter = emailFrontmatter & "---"
+  ' Make YAML Frontmatter
+  Dim emailFrontmatter As String
+  emailFrontmatter = "---" & vbCrLf
+  emailFrontmatter = emailFrontmatter & "subject: " & Chr(34) & oMail.Subject & Chr(34) & vbCrLf
+  emailFrontmatter = emailFrontmatter & "from: " & Chr(34) & sender & Chr(34) & vbCrLf
+  emailFrontmatter = emailFrontmatter & "to: " & recipString & vbCrLf
+  emailFrontmatter = emailFrontmatter & "date:" & Format(oMail.ReceivedTime, "yyyy-mm-dd") & Format(oMail.ReceivedTime, "hh:MM AM/PM") & vbCrLf
+  emailFrontmatter = emailFrontmatter & "tags: email" & vbCrLf
+  emailFrontmatter = emailFrontmatter & "---"
 
-    ' Add Frontmatter to email body
-    Dim emailContent As String
-    emailContent = emailFrontmatter & vbCrLf & vbCrLf & vbCrLf & oMail.Body
+  ' Add Frontmatter to email body
+  Dim emailContent As String
+  emailContent = emailFrontmatter & vbCrLf & vbCrLf & vbCrLf & oMail.Body
 
-    ' Make a dummy email to hold the details we're saving
-    ' This way we dont get junk in the message header when saving
-    Dim outputItem As MailItem
-      Set outputItem = Application.CreateItem(olMailItem)
-    outputItem.Body = emailContent
-    outputItem.BodyFormat = olFormatPlain
+  ' Make a dummy email to hold the details we're saving
+  ' This way we dont get junk in the message header when saving
+  Dim outputItem As MailItem
+    Set outputItem = Application.CreateItem(olMailItem)
+  outputItem.Body = emailContent
+  outputItem.BodyFormat = olFormatPlain
 
-    ' Make the file name
-    ' format like YYYY-MM-DD_email_subjectlinegoeshere.md
-    ' I used dated filenames for all notes to see them as a timeline
-    ' and because git/pcloud can destroy filedates
-    fileName = Format(dtDate, "yyyy-mm-dd", vbUseSystemDayOfWeek, vbUseSystem)
-    fileName = fileName & "_" & emailFileNameStartChr & "_"
-    fileName = fileName & temporarySubjectLineString & ".md"
+  ' Make the file name
+  ' format like YYYY-MM-DD_email_subjectlinegoeshere.md
+  ' I used dated filenames for all notes to see them as a timeline
+  ' and because git/pcloud can destroy filedates
+  fileName = Format(dtDate, "yyyy-mm-dd", vbUseSystemDayOfWeek, vbUseSystem)
+  fileName = fileName & "_" & emailFileNameStartChr & "_"
+  fileName = fileName & temporarySubjectLineString & ".md"
 
-    ' Save the result
-    outputItem.SaveAs vaultPathToSaveFileTo & fileName, OLTXT
+  ' Save the result
+  outputItem.SaveAs vaultPathToSaveFileTo & fileName, OLTXT
 
   Next
 EndClean:
@@ -120,4 +120,3 @@ EndClean:
   Set oMail = Nothing
   Set outputItem = Nothing
 End Sub
-
--- VBA Utilities/SaveUtilities.vb
+++ VBA Utilities/SaveUtilities.vb
@@ -53,7 +53,7 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
   ' This function parses the attendees and formats them to:
   ' [[@Bryan Jenks]]
   Dim typeOfNameToClean As Integer
-  
+
   ' If attendee is an outside Active Directory individual
   ' like a gmail account or external person then the display
   ' is just first and last names. these are perfect to easily format
@@ -63,7 +63,7 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
     regexJustFirstNameAndLastName.Pattern = "^\w+\s\w+$"
   If regexJustFirstNameAndLastName.Test(str) = True Then typeOfNameToClean = 1
   Set regexJustFirstNameAndLastName = Nothing
-  
+
   ' This finds emails that are `last name, first name@domain.com`
   ' not just `.com` will also pick up multiples like
   ' ex: `@domain.or.gov` etc
@@ -72,7 +72,7 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
     regexFirstNameLastNameAndFullDomain.Pattern = "^\w+,\s\w+@\w+(\.\w+)+"
   If regexFirstNameLastNameAndFullDomain.Test(str) = True Then typeOfNameToClean = 2
   Set regexFirstNameLastNameAndFullDomain = Nothing
-  
+
   ' A full and normal email only as the invited person
   ' ex: johndoe@domain.com
   Dim regexPlainEmailAddress As Object
@@ -80,7 +80,7 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
     regexPlainEmailAddress.Pattern = "^\w+@\w+\.\w+"
   If regexPlainEmailAddress.Test(str) = True Then typeOfNameToClean = 3
   Set regexPlainEmailAddress = Nothing
-  
+
   ' Active Directory people that display like:
   'ex: `Last Name, First Name (AGENCY)`
   Dim regexLastNameFirstNameAndAgency As Object
@@ -88,7 +88,7 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
     regexLastNameFirstNameAndAgency.Pattern = "^[a-zA-Z_\-]+,\s[a-zA-Z_\-]+\s\(\w+\)$"
   If regexLastNameFirstNameAndAgency.Test(str) = True Then typeOfNameToClean = 4
   Set regexLastNameFirstNameAndAgency = Nothing
-  
+
   ' Single name entity such as a distribution list or 1 word titled entity
   ' ex: `payroll (Agency)`
   Dim regexSingleNameAndDomain As Object
@@ -96,28 +96,28 @@ Public Function formatName(str As String, personNameStartChar As String) As Stri
     regexSingleNameAndDomain.Pattern = "^([a-zA-Z_\-]+\s)+\([a-zA-Z_\-]+\)"
   If regexSingleNameAndDomain.Test(str) = True Then typeOfNameToClean = 5
   Set regexSingleNameAndDomain = Nothing
-  
+
   Select Case typeOfNameToClean
     Case 1 ' John Doe
-      formatName = "[[" & personNameStartChar & str & "]]"
+      formatName = str
     Case 2 ' Doe, John@domain.or.gov
       Dim fname As String, lname As String
       fname = Mid(str, InStr(str, ", ") + 2, InStr(str, "@") - (InStr(str, ", ") + 2))
       lname = Mid(str, 1, InStr(str, ",") - 1)
       ' Assemble the building blocks and assigning to return value
-      formatName = "[[" & personNameStartChar & fname & " " & lname & "]]"
+      formatName = fname & " " & lname
     Case 3 ' JohnDoe@gmail.com
-      formatName = "[[" & personNameStartChar & Left(str, InStr(str, "@") - 1) & "]]"
+      formatName = Left(str, InStr(str, "@") - 1)
     Case 4 ' Doe, John (Agency)
       Dim fname1 As String, lname1 As String
       fname1 = Mid(str, InStr(str, ", ") + 2, InStr(str, " (") - (InStr(str, ", ") + 2))
       lname1 = Mid(str, 1, InStr(str, ",") - 1)
       ' Assemble the building blocks and assigning to return value
-      formatName = "[[" & personNameStartChar & fname1 & " " & lname1 & "]]"
+      formatName = fname1 & " " & lname1
     Case 5 ' Payroll (Agency)
-      formatName = "[[" & Left(str, InStr(str, " (") - 1) & "]]"
+      formatName = Left(str, InStr(str, " (") - 1)
     Case Else ' Anything else
-      formatName = "[[" & str & "]]"
+      formatName = str
   End Select
 
 End Function
